bd9c36a7b5f7bdf4eb89c81bd52e8f5a02e789cc
Nour <nourreda276@gmail.com>
2023-09-02 00:44:16 +0300

    changes header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Nav from "../Componant/Nav";
+import { BsHandbag } from "react-icons/bs";
 
 const Header = () => {
-  return <div>Header Skipper</div>;
+  return (
+    <div className="container mx-auto py-[50px]">
+      <div className="flex-between">
+        <h1 className="text-2xl font-semibold">board</h1>
+        <Nav />
+        <BsHandbag />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
