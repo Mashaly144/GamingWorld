3fa6e1d21010706ed8347d9abe1bc1f4fe0b83df
Nour <nourreda276@gmail.com>
2023-09-05 01:08:32 +0300

    test

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,28 +1,39 @@
-import { testimonialsCard } from '../constant/data';
-import React from 'react';
-import Swiper from 'react-id-swiper';
-import TestimonialsCard from './TestimonialsCard';
+import { testimonialsCard } from "../constant/data";
+import React from "react";
+import Swiper from "react-id-swiper";
+import TestimonialsCard from "./TestimonialsCard";
 
 const Testimonials = () => {
   const params = {
+    slidesPerView: 2,
+    spaceBetween: 30,
     pagination: {
-      el: '.swiper-pagination',
+      el: ".swiper-pagination",
       clickable: true,
-      dynamicBullets: true,
     },
   };
 
   return (
-    <div>
-      <Swiper {...params}>
-        {testimonialsCard.map((card, index) => {
-          return (
-            <div>
-              <TestimonialsCard cardData={card} />
-            </div>
-          );
-        })}
-      </Swiper>
+    <div className="relative py-[100px]">
+      <div className="container mx-auto ">
+        <h1 className="mainHeader text-center mb-[20px]">
+          Welcome To
+          <span className="bg-gradient-background text-transparent bg-clip-text">
+            Testimonials
+          </span>
+        </h1>
+        <Swiper {...params}>
+          {testimonialsCard.map((card, index) => {
+            return (
+              <div>
+                <TestimonialsCard cardData={card} />
+              </div>
+            );
+          })}
+        </Swiper>
+        <div className="overlay-circle left-0 top-0" />
+        <div className="overlay-circle right-0 top-52" />
+      </div>
     </div>
   );
 };
--- src/components/TestimonialsCard.jsx
+++ src/components/TestimonialsCard.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import { AiFillStar } from "react-icons/ai";
-
+import verified from "../assets/verified.png";
 const TestimonialsCard = ({ cardData }) => {
   return (
-    <div className="container">
+    <div>
       <div className="flex-between">
-        <div className=" gap-3">
-          <div className="flex">
+        <div className="space-y-5 main-testimonial">
+          <div className="flex  text-2xl items-center">
+            <AiFillStar />
             <AiFillStar />
             <AiFillStar />
             <AiFillStar />
             <AiFillStar />
           </div>
-          <div>{cardData.description}</div>
-          <div className="flex-between">
-            <div className="flex gap-5">
+          <h1 className="text-base font-normal text-gray-200">
+            {cardData.description}
+          </h1>
+          <div className="flex-between ">
+            <div className="flex gap-5 items-center">
               <img src={cardData.img} />
-              <h1>{cardData.userName}</h1>
-              <p>{cardData.Career}</p>
+              <div className="flex flex-col">
+                <h1>{cardData.userName}</h1>
+                <p className="text-gray-400 font-thin">{cardData.Career}</p>
+              </div>
+            </div>
+            <div className="flex items-center gap-2">
+              <img src={verified} />
+              <h2>Verified</h2>
             </div>
           </div>
         </div>
