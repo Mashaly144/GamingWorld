135faf130bc920c027263a9cd026fd3314f5ffd8
mashaly <abdallahmashaly144@gmail.com>
2023-09-05 01:11:53 +0300

    favorite card with swiper done

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { games } from '../constant/data';
 import Card from './Card';
+import Button from '../ui/Button';
 const Articles = () => {
   return (
     <div className='relative py-[100px]'>
       <div className='container mx-auto'>
         <div>
-          <h1 className='text-5xl text-center py-7 font-semibold capitalize'>
+          <h1 className='text-5xl text-center font-semibold capitalize'>
             Welcome to the top games
           </h1>
+          <div className='flex-between capitalize mx-auto py-[80px] w-[80%] gap-4'>
+            <Button styles={`main-button-active`}>Mewest games</Button>
+            <Button styles={`main-button-active`}>Latest games</Button>
+            <Button styles={`main-button-active`}>Fight games</Button>
+            <Button styles={`main-button-active`}>sport games</Button>
+          </div>
           <div className='card-wrapper'>
             {games.map((user, index) => {
               return <Card user={user} />;
--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -8,16 +8,17 @@ import game3 from '../assets/game3.png';
 import game4 from '../assets/game4.png';
 import game5 from '../assets/game5.png';
 import game6 from '../assets/game6.png';
+import Button from '../ui/Button';
 const Favorites = () => {
   const params = {
     effect: 'coverflow',
     grabCursor: true,
     centeredSlides: true,
-    slidesPerView: 'auto',
+    slidesPerView: '2',
     coverflowEffect: {
       rotate: 50,
       stretch: 0,
-      depth: 100,
+      depth: 50,
       modifier: 1,
       slideShadows: true,
     },
@@ -27,10 +28,10 @@ const Favorites = () => {
   };
 
   return (
-    <div>
+    <div className='py-[50px] relative'>
       <div className='container mx-auto'>
         <div>
-          <div className='flex flex-col justify-center items-center gap-6 '>
+          <div className='flex flex-col justify-center items-center gap-6 pb-[80px]'>
             <h1 className='mainHeader text-center'>
               choose your
               <span className='bg-gradient-background text-transparent bg-clip-text'>
@@ -51,6 +52,12 @@ const Favorites = () => {
             <img src={game5} alt='game' />
             <img src={game6} alt='game' />
           </Swiper>
+          <div className='flex-between mx-auto pt-[80px] w-[50%] gap-4'>
+            <Button styles={`main-button-active`}>Buy now</Button>
+            <Button styles={`main-button`}>Play now</Button>
+          </div>
+          <div className='overlay-circle left-0 top-0' />
+          <div className='overlay-circle right-0 bottom-0' />
         </div>
       </div>
     </div>
